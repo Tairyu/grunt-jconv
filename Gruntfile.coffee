e73e740422217c8b23926185e8a246bb25250e0a
ta_shimizu <ta_shimizu@waku-2.com>
2014-08-06 16:49:21 +0900

    jshint, jscs,jsfmt を導入

--- Gruntfile.coffee
+++ Gruntfile.coffee
@@ -1,6 +1,7 @@
 module.exports = (grunt) ->
   require('time-grunt') grunt
-  require('jit-grunt') grunt
+  require('jit-grunt') grunt,
+    jscs: 'grunt-jscs-checker'
 
   grunt.initConfig
     pkg: grunt.file.readJSON 'package.json'
@@ -34,6 +35,22 @@ module.exports = (grunt) ->
           toEncode: 'SJIS'
         src: 'dist/src3/*.txt' # dist path is own file.
 
+    shell:
+      jsfmt:
+        command: 'jsfmt -w tasks/*.js'
+
+    jscs:
+      options:
+        config: '.jscsrc'
+        force: true
+      src: 'tasks/*.js'
+
+    jshint:
+      options:
+        jshintrc: '.jshintrc'
+      src: 'tasks/*.js'
+
 
   grunt.loadTasks 'tasks'
   grunt.registerTask 'default', ['jconv']
+  grunt.registerTask 'lint', ['shell:jsfmt', 'jshint', 'jscs']
--- tasks/jconv.js
+++ tasks/jconv.js
@@ -4,40 +4,42 @@ var path = require('path');
 var fs = require('fs');
 var jconv = require('jconv');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  var curDest = function(dest, srcPath){
-    if(!dest) return srcPath;
+  var curDest = function (dest, srcPath) {
+    if (!dest) return srcPath;
 
-    if(path.extname(dest) === ''){
+    if (path.extname(dest) === '') {
       grunt.file.mkdir(dest);
       return path.join(dest, path.basename(srcPath));
-    }
-    else {
+    } else {
       return dest;
     }
   };
 
-  grunt.registerMultiTask('jconv', 'Convert files with jconv.', function() {
+  grunt.registerMultiTask('jconv', 'Convert files with jconv.', function () {
 
-    var options = this.options({fromEncode: 'UTF8', toEncode: 'UTF8'});
+    var options = this.options({
+      fromEncode: 'UTF8',
+      toEncode: 'UTF8'
+    });
 
-    this.files.forEach(function(file){
+    this.files.forEach(function (file) {
 
       file.src
-        .filter(function(srcPath){
-          if(!grunt.file.exists(srcPath)) {
-            grunt.log.warn('src file "' + srcPath + '" not found.');
+      .filter(function (srcPath) {
+        if (!grunt.file.exists(srcPath)) {
+          grunt.log.warn('src file "' + srcPath + '" not found.');
 
-            return false;
-          }
+          return false;
+        }
 
-          return true;
-        })
-        .map(function(srcPath){
-          grunt.file.write(
-            curDest(file.dest, srcPath),
-            jconv(fs.readFileSync(srcPath), options.fromEncode, options.toEncode));
+        return true;
+      })
+      .map(function (srcPath) {
+        grunt.file.write(
+        curDest(file.dest, srcPath),
+        jconv(fs.readFileSync(srcPath), options.fromEncode, options.toEncode));
       });
     });
   });
